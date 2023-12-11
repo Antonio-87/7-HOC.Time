6770e64400943bc9157432c60fa875b063b5e8a5
Antonio-87 <davydenko.87@list.ru>
2023-12-11 23:08:25 +0300

    no_commit_DatetimePretty_class

--- time/src/components/DateTimePretty.tsx
+++ time/src/components/DateTimePretty.tsx
@@ -24,34 +24,5 @@ const DateTimePretty = (WrappedComponent: ComponentType<{ date: string }>) => {
     return <WrappedComponent date={formattedDate} />;
   };
 };
-// (date: string) => (Component: React.ComponentType<{ date: string }>) =>
-//   class extends React.Component {
-//     static get displayName() {
-//       const name = Component.displayName || Component.name || "Component";
-//       return `WithData(${name})`;
-//     }
-
-//     componentDidMount(): void {
-//       this.formatDate(date);
-//     }
-
-//     formatDate = (date: string) => {
-//       const endDate = new Date(date);
-//       const currentDate = new Date();
-//       const differenceTime = currentDate.getTime() - endDate.getTime();
-//       const differenceDate = new Date(differenceTime);
-//       const formatTime =
-//         differenceTime / 60000 >= 60 * 24
-//           ? `${differenceDate.getDay()} дней назад`
-//           : differenceTime / 60000 < 60
-//             ? `${differenceDate.getHours()} часов наза`
-//             : `${differenceDate.getMinutes()} минут наза`;
-//       this.setState(formatTime);
-//     };
-
-//     render(): React.ReactNode {
-//       return <Component date={date} {...this.state} />;
-//     }
-// };
 
 export default DateTimePretty;
